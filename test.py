d68f1322a29232e810bc7a7668d69a09a662ee44
Francis Behnen <francis.behnen@gmail.com>
2021-06-17 10:15:44 +0200

    Setup local in docker (#2)
    * Update dockerfile to copy everything
    
    * Change common fork
    
    * Add default env vars to start script
    
    * Correct git commands in start script
    
    * Update dockerfile and start script for local
    
    * Update readme with explanation on how to use and add Dockerfile and test.py for convenience
    
    * Bugfix
    
    * Remove Tensorflow and pandas as they're not needed
    
    * Open ports and some readme clarifications

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from cloudburst.client.client import CloudburstConnection
-local_cloud = CloudburstConnection('127.0.0.1', 'host.docker.internal', local=True)
+local_cloud = CloudburstConnection('127.0.0.1', '127.0.0.1', local=True)
 cloud_sq = local_cloud.register(lambda _, x: x * x, 'square')
 print(cloud_sq(2).get())
 print(local_cloud.register_dag('dag', ['square'], []))
